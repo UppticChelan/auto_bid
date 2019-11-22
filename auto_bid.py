2e036a8e9b18a227bc400bd84a3b5b25d8eaadf6
Chelan Patton <chelanpatton@Upptics-MacBook-Pro.local>
2019-11-21 17:37:01 -0800

    Update script to include country handling

--- auto_bid.py
+++ auto_bid.py
@@ -3,31 +3,33 @@ import pandas as pd
 def main(argv):
     input_file = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hi:p:",["ifile=","percent="])
     except getopt.GetoptError:
         print ('auto_bid.py -i <inputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('auto_bid.py -i <inputfile>')
+            print ('auto_bid.py -i <inputfile> -p <target_percent_roas>')
             sys.exit()
-        elif opt in ("-i", "--ifile"):
+        elif opt in ('-i', '--ifile'):
             input_file = arg
+        elif opt in ('-p', '--percent'):
+            target_roas = float(arg)
     df = pd.read_csv(input_file)
-    baselines = df.groupby(['campaign_name']).sum().reset_index()
-    df['unadjusted_bid'] = df.apply(lambda x: get_baselines(x['iap_revenue_7d'], x['installs'], 0.6), axis=1)
-    baselines['base_bid'] = baselines.apply(lambda x: get_baselines(x['iap_revenue_7d'], x['installs'], 0.6), axis=1)
+    baselines = df.groupby(['campaign_name', 'country_name']).sum().reset_index()
+    df['unadjusted_bid'] = df.apply(lambda x: get_baselines(x['iap_revenue_7d'], x['installs'], target_roas), axis=1)
+    baselines['base_bid'] = baselines.apply(lambda x: get_baselines(x['iap_revenue_7d'], x['installs'], target_roas), axis=1)
     df = df.join(baselines[['campaign_name', 'base_bid']].set_index('campaign_name'), on='campaign_name')
     df['final_bid_value'] = df.apply(lambda x: apply_bid_logic(x['unadjusted_bid'], x['installs'], x['base_bid']), axis=1)
     df = df.round(2)
     campaigns = df['campaign_id'].unique()
     for campaign in campaigns:
-        subset = df[df['campaign_id'] == campaign][['campaign_id','campaign_name', 'application_id', 'application_name', 'final_bid_value']]
-        subset['Country'] = 'US'
+        subset = df[df['campaign_id'] == campaign][['campaign_id','campaign_name', 'application_id', 'application_name', 'country_name','final_bid_value']]
         subset_renamed = subset.rename(columns={'campaign_id':'Campaign ID',
                                            'campaign_name':'Campaign Name',
                                            'application_id':'Application ID',
                                            'application_name':'Application name',
+                                            'country_name':'Country',
                                             'final_bid_value':'Bid'
                                            })
         campaign_name = subset_renamed['Campaign Name'].iloc[0]
