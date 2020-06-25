f9f4f02df3425ce8db80bf34fb786c043514f446
Chelan Patton <chelanpatten@Upptic-Analytics-MacBook-Pro-001.local>
2020-06-25 15:44:14 -0700

    Fix for BigQuery formatting

--- ruleset.py
+++ ruleset.py
@@ -24,7 +24,7 @@ def format_cols_input(df, ruleset):
     if ruleset.input == 'acquired':
         df = df.rename(columns={"Campaign": "Campaign Name"})
     if ruleset.input == 'big_query':
-        df = df.rename(columns={"tracker_campaign_name": "Campaign Name", "tracker_campaign_id": "Campaign ID", "country_field":"Country", "clean_subpub":"Application ID", "d7":"d7_total_revenue", "installs":"Installs"})
+        df = df.rename(columns={"tracker_campaign_name": "Campaign Name", "tracker_campaign_id": "Campaign ID", "country_field":"Country", "publisher_id":"Application ID", "d7":"d7_total_revenue", "installs":"Installs"})
         df['Country'] = df['Country'].str[:2]
     return df
 
